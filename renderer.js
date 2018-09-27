6d85c27fa3af9c4e1ee5a0adcc4932fc9fa5c931
David LeGare <excaliburhissheath@gmail.com>
2018-09-27 13:37:08 -0500

    Open a single window on start

--- renderer.js
+++ renderer.js
@@ -6,19 +6,22 @@ let app = new Vue({
     components: {
         VueJsonPretty,
     },
+
     data: {
+        // Capture data about the Electron process so that we can display it in the app if we want.
         process: process,
-        entities: [],
-        components: [],
-        resources: [],
-        rawComponents: null,
-        selectedEntity: null,
+
+        // A map containing the data for each game currently connected to the editor.
+        games: {},
+
+        // The list of tabs that are available for each game. Each game tracks its own state for
+        // which tab is currently selected.
         tabs: [
             'Entities',
             'Resources',
         ],
-        activeTab: 0,
     },
+
     methods: {
         selectEntity: function(entity) {
             this.selectedEntity = entity;
@@ -31,22 +34,49 @@ let app = new Vue({
 });
 exports.app = app;
 
-ipcRenderer.on('message', (event, data) => {
-    app.entities = data.entities;
+ipcRenderer.on('connect', (event, data) => {
+    app.games[data.id] = {
+        entities: [],
+        components: [],
+        resources: [],
+        rawComponents: null,
+        selectedEntity: null,
+        activeTab: 0,
 
-    // Sort components before updating the local data to ensure that components always appear
-    // in the same order regardless of the order they are sent in.
-    var sortedComponents = data.components;
-    sortedComponents.sort(compareNamed);
-    app.components = sortedComponents;
+        update: function(data) {
+            this.entities = data.entities;
 
-    // Sort resources before updating the local data to ensure that resources always appear
-    // in the same order regardless of the order they are sent in.
-    var sortedResources = data.resources;
-    sortedResources.sort(compareNamed);
-    app.resources = sortedResources;
+            // Sort components before updating the local data to ensure that components always appear
+            // in the same order regardless of the order they are sent in.
+            var sortedComponents = data.components;
+            sortedComponents.sort(compareNamed);
+            this.components = sortedComponents;
+
+            // Sort resources before updating the local data to ensure that resources always appear
+            // in the same order regardless of the order they are sent in.
+            var sortedResources = data.resources;
+            sortedResources.sort(compareNamed);
+            this.resources = sortedResources;
+        },
+    };
 });
 
+ipcRenderer.on('disconnect', (event, data) => {
+    delete app.games[data.id];
+});
+
+ipcRenderer.on('update', (event, data) => {
+    console.log(data);
+
+    let game = app.games[data.id];
+    game.update(data.data);
+});
+
+/**
+ * Compares two objects by name, returning a numeric value based on their relative ordering.
+ *
+ * Useful for sorting a list of objects by their name, rather than their natural ordering.
+ */
 function compareNamed(left, right) {
     if (left.name < right.name) { return -1; }
     if (left.name > right.name) { return 1; }
