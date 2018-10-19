97a49e893236f2a4aa466d7b6891622bf400b08f
David LeGare <excaliburhissheath@gmail.com>
2018-10-18 22:23:30 -0500

    Add nested-data component

--- components/data-display.js
+++ components/data-display.js
@@ -1,11 +1,53 @@
 Vue.component('data-display', {
     props: {
         data: [Object, Array],
-        isRoot: {
-            type: Boolean,
-            default: true,
-        },
     },
+
+    computed: {
+        hasEdits: function() {
+            if (this.$refs.nestedData != null) {
+                return this.$refs.nestedData.hasEdits;
+            }
+
+            return false;
+        },
+
+        editedValue: function() {
+            return this.$refs.nestedData.editedValue;
+        }
+    },
+
+    methods: {
+        submitEdits: function() {
+            this.$emit('submit', this.editedValue);
+            this.clearEdits();
+        },
+
+        clearEdits: function() {
+            this.edits = {};
+            if (this.$refs.nestedData != null) {
+                this.$refs.nestedData.clearEdits();
+            }
+        }
+    },
+
+    template: `
+        <form v-on:submit.prevent="submitEdits()" class="data-display">
+            <nested-data
+                :data="data"
+                ref="nestedData"
+            ></nested-data>
+
+            <input
+                type="submit"
+                class="button is-fullwidth is-primary"
+            >
+        </form>
+    `,
+});
+
+Vue.component('nested-data', {
+    props: ['data'],
 
     data: function() {
         return {
@@ -33,11 +75,6 @@ Vue.component('data-display', {
     },
 
     methods: {
-        applyEdits: function() {
-            this.$emit('submit', this.editedValue);
-            this.clearEdits();
-        },
-
         clearEdits: function() {
             this.edits = {};
             if (this.$refs.nestedData != null) {
@@ -49,55 +86,41 @@ Vue.component('data-display', {
     },
 
     template: `
-        <form v-on:submit.prevent="" class="data-display" v-bind:class="{ 'is-root': isRoot }">
-            <ul>
-                <li v-for="(value, key) in data">
-
-                    <template v-if="value instanceof Object || value instanceof Array">
-                        <label class="label">{{ key }}</label>
-                        <data-display
-                            :data="value"
-                            v-model="edits[key]"
-                            v-on:input="$emit('input', editedValue)"
-                            v-on:submit.prevent="applyEdits()"
-                            ref="nestedData"
-                            :isRoot="false"
-                        ></data-display>
-                    </template>
-                    <template v-else>
-                        <div class="field is-horizontal">
-                            <div class="field-label">
-                                <label class="label">{{ key }}</label>
-                            </div>
-                            <input
-                                v-if="typeof value === 'number'"
-                                v-model.number="edits[key]"
-                                v-bind:placeholder="value"
-                                v-on:input="$emit('input', editedValue)"
-                                v-on:submit.prevent="applyEdits()"
-                                type="number"
-                                class="input"
-                            >
-                            <input
-                                v-else
-                                v-model="edits[key]"
-                                v-bind:placeholder="value"
-                                v-on:input="$emit('input', editedValue)"
-                                v-on:submit.prevent="applyEdits()"
-                                class="input"
-                            >
+        <ul>
+            <li v-for="(value, key) in data">
+                <template v-if="value instanceof Object || value instanceof Array">
+                    <label class="label">{{ key }}</label>
+                    <nested-data
+                        :data="value"
+                        v-model="edits[key]"
+                        v-on:input="$emit('input', editedValue)"
+                        ref="nestedData"
+                        :isRoot="false"
+                    ></nested-data>
+                </template>
+                <template v-else>
+                    <div class="field is-horizontal">
+                        <div class="field-label">
+                            <label class="label">{{ key }}</label>
                         </div>
-                    </template>
-                </li>
-            </ul>
-
-            <button
-                v-if="isRoot && hasEdits"
-                v-on:click="applyEdits()"
-                class="button is-fullwidth is-primary"
-            >
-                Apply
-            </button>
-        </form>
+                        <input
+                            v-if="typeof value === 'number'"
+                            v-model.number="edits[key]"
+                            v-bind:placeholder="value"
+                            v-on:input="$emit('input', editedValue)"
+                            type="number"
+                            class="input"
+                        >
+                        <input
+                            v-else
+                            v-model="edits[key]"
+                            v-bind:placeholder="value"
+                            v-on:input="$emit('input', editedValue)"
+                            class="input"
+                        >
+                    </div>
+                </template>
+            </li>
+        </ul>
     `,
 });
